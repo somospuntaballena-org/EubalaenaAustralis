36a76226777c11f8d142c36401e16c0ddf05fae1
Marcelo Forets <marcelo@planting.space>
2024-03-03 01:01:29 +0100

    formatter

--- extract_kml_info.py
+++ extract_kml_info.py
@@ -11,6 +11,7 @@ output_dir = 'output'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
+
 def clear_output_directory(directory='output'):
     # Check if the directory exists.
     if os.path.exists(directory):
@@ -24,6 +25,7 @@ def clear_output_directory(directory='output'):
     else:
         print(f"The directory {directory} does not exist.")
 
+
 def fetch_and_save_kml(url, save_path):
     response = requests.get(url)
     if response.status_code == 200:
@@ -33,6 +35,7 @@ def fetch_and_save_kml(url, save_path):
         print(f"File saved successfully to {save_path}")
     else:
         print("Failed to fetch KML content")
+
 
 def extract_info_from_network_link_and_save(kml_file_path):
     # Namespace for parsing.
@@ -51,17 +54,21 @@ def extract_info_from_network_link_and_save(kml_file_path):
         # Generate save_path with current date and time.
         current_datetime = datetime.now().strftime("%Y_%m_%d_%Hh_%Mm_%Ss")
         save_path = f"{kml_file_path.rsplit('.', 1)[0]}_{current_datetime}.kml"
-        
+
         fetch_and_save_kml(href, save_path)
     else:
         print("No NetworkLink found")
     return save_path
 
-# Function to perform geocoding using Nominatim (OpenStreetMap).
+
 def geocode_coordinates(lat, lon):
+    '''
+    Function to perform geocoding using Nominatim (OpenStreetMap).
+    '''
     try:
         # Construct request URL
-        url = f"https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}&format=json"
+        url = f"https://nominatim.openstreetmap.org/reverse?lat={
+            lat}&lon={lon}&format=json"
         response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
         if response.status_code == 200:
             data = response.json()
@@ -72,17 +79,19 @@ def geocode_coordinates(lat, lon):
     except Exception as e:
         return f"Exception: {e}"
 
+
 def df_to_html_table(df, file_name):
     html_table = df.to_html(index=False)
     with open(os.path.join(output_dir, file_name), 'w') as f:
         f.write(html_table)
     print(f"HTML table saved to {os.path.join(output_dir, file_name)}")
 
+
 def markdown_to_pdf(markdown_file, pdf_file, margin='2cm'):
     try:
         subprocess.run([
-            'pandoc', 
-            os.path.join(output_dir, markdown_file), 
+            'pandoc',
+            os.path.join(output_dir, markdown_file),
             '-o', os.path.join(output_dir, pdf_file),
             '--pdf-engine=xelatex',
             '-V', f'geometry:margin={margin}'
@@ -91,11 +100,13 @@ def markdown_to_pdf(markdown_file, pdf_file, margin='2cm'):
     except subprocess.CalledProcessError as e:
         print(f"Error during PDF generation: {e}")
 
+
 def df_to_markdown(df, file_name):
     markdown_content = df.to_markdown(index=False)
     with open(os.path.join(output_dir, file_name), 'w') as f:
         f.write(markdown_content)
     print(f"Markdown saved to {os.path.join(output_dir, file_name)}")
+
 
 def extract_info_and_geocode(kml_file_path):
     ns = {'kml': 'http://www.opengis.net/kml/2.2'}
@@ -105,18 +116,22 @@ def extract_info_and_geocode(kml_file_path):
     data = []
     placemarks = root.findall('.//kml:Placemark', ns)
     for placemark in placemarks:
-        name = placemark.find('kml:name', ns).text if placemark.find('kml:name', ns) is not None else ''
-        description = placemark.find('kml:description', ns).text if placemark.find('kml:description', ns) is not None else ''
-        coord_text = placemark.find('.//kml:coordinates', ns).text if placemark.find('.//kml:coordinates', ns) is not None else ''
+        name = placemark.find('kml:name', ns).text if placemark.find(
+            'kml:name', ns) is not None else ''
+        description = placemark.find('kml:description', ns).text if placemark.find(
+            'kml:description', ns) is not None else ''
+        coord_text = placemark.find('.//kml:coordinates', ns).text if placemark.find(
+            './/kml:coordinates', ns) is not None else ''
         if coord_text:
             print(f"Found coordinates {coord_text} for placemark {placemark}")
             lon, lat, _ = map(float, coord_text.split(','))
             address = geocode_coordinates(lat, lon)
         else:
             address = "Coordinates not found"
-        
-        data.append({'Nombre': name, 'Dirección': address, 'Notas': description})
-  
+
+        data.append(
+            {'Nombre': name, 'Dirección': address, 'Notas': description})
+
     df = pd.DataFrame(data)
 
     # Saving to CSV with the same base name as the KML file
@@ -124,7 +139,7 @@ def extract_info_and_geocode(kml_file_path):
     csv_file_path = f"{base_name}.csv"
     df.to_csv(os.path.join(output_dir, csv_file_path), index=False)
     print(f"DataFrame saved to {os.path.join(output_dir, csv_file_path)}")
-    
+
     # Saving to HTML table.
     html_file_path = f"{base_name}.html"
     df_to_html_table(df, html_file_path)
@@ -142,6 +157,7 @@ def extract_info_and_geocode(kml_file_path):
     print(f"PDF saved to {os.path.join(output_dir, pdf_file_path)}")
 
     return df
+
 
 # Generate output files.
 kml_network_link = 'firmas.kml'
